79d4d8b776cf82d8027c755d26e9a89ecda428ee
Cristian Guilarte <159936410+CrisGui@users.noreply.github.com>
2024-09-05 13:11:01 +0100

    fix(neovim): Fix plugins path directory

--- neovim/.config/nvim/lua/user/core/plugin.lua
+++ neovim/.config/nvim/lua/user/core/plugin.lua
@@ -1,16 +1,16 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		vim.api.nvim_echo({
-			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-			{ out, "WarningMsg" },
-			{ "\nPress any key to exit..." },
-		}, true, {})
-		vim.fn.getchar()
-		os.exit(1)
-	end
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -19,83 +19,83 @@ vim.opt.rtp:prepend(lazypath)
 -- This is also a good place to setup other settings (vim.opt)
 
 local opts = {
-	root = vim.fn.stdpath("data") .. "/lazy", -- directory where plugins will be installed
-	defaults = {
-		lazy = true,
-		version = nil,
-		cond = vim.g.vscode,
-	},
-	--spec = { import = "user/plugins" },
-	local_spec = true,
-	lockfile = vim.fn.stdpath("config") .. "/lazy-lock.json",
-	concurrency = jit.os:find("Windows") and (vim.uv.available_parallelism() * 2) or nil,
-	git = {
-		log = { "-8" },
-		timeout = 120,
-		url_format = "https://github.com/%s.git",
-		filter = true,
-	},
-	pkg = {
-		enabled = true,
-		cache = vim.fn.stdpath("state") .. "/lazy/pkg-cache.lua",
-		versions = true,
-		sources = {
-			"lazy",
-			"rockspec",
-			"packspec",
-		},
-	},
-	rocks = {
-		root = vim.fn.stdpath("data") .. "/lazy-rocks",
-		server = "https://nvim-neorocks.github.io/rocks-binaries/",
-	},
-	--[[
+  root = vim.fn.stdpath("data") .. "/lazy", -- directory where plugins will be installed
+  defaults = {
+    lazy = true,
+    version = nil,
+    cond = vim.g.vscode,
+  },
+  spec = { import = "user/plugins" },
+  local_spec = true,
+  lockfile = vim.fn.stdpath("config") .. "/lazy-lock.json",
+  concurrency = jit.os:find("Windows") and (vim.uv.available_parallelism() * 2) or nil,
+  git = {
+    log = { "-8" },
+    timeout = 120,
+    url_format = "https://github.com/%s.git",
+    filter = true,
+  },
+  pkg = {
+    enabled = true,
+    cache = vim.fn.stdpath("state") .. "/lazy/pkg-cache.lua",
+    versions = true,
+    sources = {
+      "lazy",
+      "rockspec",
+      "packspec",
+    },
+  },
+  rocks = {
+    root = vim.fn.stdpath("data") .. "/lazy-rocks",
+    server = "https://nvim-neorocks.github.io/rocks-binaries/",
+  },
+  --[[
   dev = {
     path = "~/projects",
     patterns = {}, -- For example {"folke"}
     fallback = false, -- Fallback to git when local plugin doesn't exist
   },
   --]]
-	install = {
-		missing = true,
-		--colorscheme = { "catppuccin" },
-	},
-	ui = {
-		size = { width = 0.8, height = 0.8 },
-		wrap = true,
-		border = "rounded",
-		backdrop = 60,
-		title = nil,
-		title_pos = "center",
-		pills = true,
-		icons = {
-			cmd = " ",
-			config = "",
-			event = " ",
-			favorite = " ",
-			ft = " ",
-			init = " ",
-			import = " ",
-			keys = " ",
-			lazy = "󰒲 ",
-			loaded = "●",
-			not_loaded = "○",
-			plugin = " ",
-			runtime = " ",
-			require = "󰢱 ",
-			source = " ",
-			start = " ",
-			task = "✔ ",
-			list = {
-				"●",
-				"➜",
-				"★",
-				"‒",
-			},
-		},
-		browser = nil,
-		throttle = 20,
-		--[[
+  install = {
+    missing = true,
+    --colorscheme = { "catppuccin" },
+  },
+  ui = {
+    size = { width = 0.8, height = 0.8 },
+    wrap = true,
+    border = "rounded",
+    backdrop = 60,
+    title = nil,
+    title_pos = "center",
+    pills = true,
+    icons = {
+      cmd = " ",
+      config = "",
+      event = " ",
+      favorite = " ",
+      ft = " ",
+      init = " ",
+      import = " ",
+      keys = " ",
+      lazy = "󰒲 ",
+      loaded = "●",
+      not_loaded = "○",
+      plugin = " ",
+      runtime = " ",
+      require = "󰢱 ",
+      source = " ",
+      start = " ",
+      task = "✔ ",
+      list = {
+        "●",
+        "➜",
+        "★",
+        "‒",
+      },
+    },
+    browser = nil,
+    throttle = 20,
+    --[[
     custom_keys = {
       ["<localleader>l"] = {
         function(plugin)
@@ -116,57 +116,57 @@ local opts = {
       },
     },
     --]]
-	},
-	diff = {
-		cmd = "git",
-	},
-	checker = {
-		enabled = true,
-		concurrency = 1,
-		notify = true,
-		frequency = 3600,
-		check_pinned = false,
-	},
-	change_detection = {
-		enabled = true,
-		notify = false,
-	},
-	performance = {
-		cache = {
-			enabled = true,
-		},
-		reset_packpath = true,
-		rtp = {
-			reset = true,
-			paths = {},
-			disabled_plugins = {
-				"spec",
-				"man",
-				"shada",
-				"rplugin",
-				"gzip",
-				"matchit",
-				"matchparen",
-				--"netrwPlugin",
-				"tarPlugin",
-				"tohtml",
-				"tutor",
-				"zipPlugin",
-			},
-		},
-	},
-	readme = {
-		enabled = true,
-		root = vim.fn.stdpath("state") .. "/lazy/readme",
-		files = { "README.md", "lua/**/README.md" },
-		-- only generate markdown helptags for plugins that dont have docs
-		skip_if_doc_exists = true,
-	},
-	state = vim.fn.stdpath("state") .. "/lazy/state.json",
-	profiling = {
-		loader = false,
-		require = false,
-	},
+  },
+  diff = {
+    cmd = "git",
+  },
+  checker = {
+    enabled = true,
+    concurrency = 1,
+    notify = true,
+    frequency = 3600,
+    check_pinned = false,
+  },
+  change_detection = {
+    enabled = true,
+    notify = false,
+  },
+  performance = {
+    cache = {
+      enabled = true,
+    },
+    reset_packpath = true,
+    rtp = {
+      reset = true,
+      paths = {},
+      disabled_plugins = {
+        "spec",
+        "man",
+        "shada",
+        "rplugin",
+        "gzip",
+        "matchit",
+        "matchparen",
+        --"netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+  readme = {
+    enabled = true,
+    root = vim.fn.stdpath("state") .. "/lazy/readme",
+    files = { "README.md", "lua/**/README.md" },
+    -- only generate markdown helptags for plugins that dont have docs
+    skip_if_doc_exists = true,
+  },
+  state = vim.fn.stdpath("state") .. "/lazy/state.json",
+  profiling = {
+    loader = false,
+    require = false,
+  },
 }
 
-require("lazy").setup({import = "user/plugins"}, opts)
+require("lazy").setup(opts)
