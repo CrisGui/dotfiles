d6adbaa57ff2edbeac3e465c09c69ecdbbe68bf7
Cristian Guilarte <159936410+CrisGui@users.noreply.github.com>
2024-09-05 12:57:40 +0100

    feat(neovim): Add init file

---
+++ neovim/.config/nvim/init.lua
@@ -0,0 +1,24 @@
+local profile = "user"
+
+if vim.loader then
+	vim.loader.enable()
+end
+
+--[[
+-- Debugging
+<F12>       Start Debugging
+<F11>       Anonymous Breakpoint
+<F10>       Verbose Breakpoint
+<F9>        Values Preview
+<F8>        Terminate
+-- LSP
+<F7>        Rename references
+<F6>        Format code
+<F5>        Code actions
+-- Testing
+<F4>        Test Last
+<F3>        Test Nearest
+<F2>        Test File
+-- UNDOTREE
+<F1>        Toggle Tree
+--]]
