2dff814b4daa794a0fdc12786095853a8ce07b7f
Cristian Guilarte <159936410+CrisGui@users.noreply.github.com>
2024-09-12 14:58:57 +0100

    chore(neovim-lsp): Uncomment code

--- neovim/.config/nvim/lua/user/plugins/luasnip.lua
+++ neovim/.config/nvim/lua/user/plugins/luasnip.lua
@@ -1,17 +1,16 @@
-return {}
--- return {
--- 	{
--- 		-- Dependency
--- 		main = "luasnip",
--- 		"L3MON4D3/LuaSnip",
--- 		version = "v2.*",
--- 		build = "make install_jsregexp",
--- 		dependencies = { "rafamadriz/friendly-snippets" },
--- 		config = function()
--- 			require("luasnip").config.set_config({
--- 				enable_autosnippets = true,
--- 			})
--- 			require("luasnip.loaders.from_lua").lazy_load({ paths = vim.fn.stdpath("config") .. "/lua/user/snippets" })
--- 		end,
--- 	},
--- }
+return {
+	{
+		-- Dependency
+		main = "luasnip",
+		"L3MON4D3/LuaSnip",
+		version = "v2.*",
+		build = "make install_jsregexp",
+		dependencies = { "rafamadriz/friendly-snippets" },
+		config = function()
+			require("luasnip").config.set_config({
+				enable_autosnippets = true,
+			})
+			require("luasnip.loaders.from_vscode").lazy_load({ paths = vim.fn.stdpath("config") .. "/lua/user/snippets" })
+		end,
+	},
+}
